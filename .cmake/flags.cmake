9c4e272be44df32448f50286bc1170e989e80564
Vincent Cruz <mooz@blockos.org>
2026-02-20 17:28:05 +0100

    Fix build for GCC > 13
    Force the detection of __VA_OPT__ or __VA_ARG__ GNU extension so that
    LMU_PP_VA_ARGS_NARG returns 0 when called without any argument.
    
    Signed-off-by: Vincent Cruz <mooz@blockos.org>
    
    ---
    https://github.com/microchip-ung/sw-mepa/pull/6
    branch: BlockoS:master

--- .cmake/flags.cmake
+++ .cmake/flags.cmake
@@ -9,7 +9,7 @@ string (REPLACE " -" ";-" SHARED_LINKER_FLAGS   "${CMAKE_SHARED_LINKER_FLAGS}")
 string (REPLACE " -" ";-" C_FLAGS               "${CMAKE_C_FLAGS}")
 string (REPLACE " -" ";-" EXE_LINKER_FLAGS      "${CMAKE_EXE_LINKER_FLAGS}")
 
-LIST(APPEND C_FLAGS   "-Wall -Wno-unknown-pragmas -Wlogical-op -Wno-array-bounds -Wno-stringop-overflow -fasynchronous-unwind-tables -std=c11 -D_POSIX_C_SOURCE=200809L -D_BSD_SOURCE -D_DEFAULT_SOURCE -ldl")
+LIST(APPEND C_FLAGS   "-Wall -Wno-unknown-pragmas -Wlogical-op -Wno-array-bounds -Wno-stringop-overflow -fasynchronous-unwind-tables -D_POSIX_C_SOURCE=200809L -D_BSD_SOURCE -D_DEFAULT_SOURCE -ldl")
 
 # This is always set by buildroot - not sure why
 LIST(REMOVE_ITEM C_FLAGS   "-Os")
